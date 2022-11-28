8b90e0f36c6f115ca5e0f09b85cd27bd5e19f4e2
gkocaman <ggokhankocaman@gmail.com>
2022-11-28 08:10:25 +0300

    TestNG suite ,xml and convert

--- src/test/day1.java
+++ src/test/day1.java
@@ -5,8 +5,12 @@ import org.testng.annotations.Test;
 public class day1 {
 
 	@Test
-	public void name() {
-		System.out.println("deneme1");
+	public void firstTest() {
+		System.out.println("Hello");
+	}
+	@Test
+	public void secondTest() {
+		System.out.println("By");
 	}
 
 }
---
+++ src/test/day2.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+	@Test
+	public void good() {
+		
+		System.out.println("Good Bro!");
+		
+	}
+
+}
