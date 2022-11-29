aa46435c1e52405e684ff9dfe4da6238c31309e5
gkocaman <ggokhankocaman@gmail.com>
2022-11-30 00:03:01 +0300

    newDay VScode integression

--- src/test/day1.java
+++ src/test/day1.java
@@ -3,22 +3,21 @@ package test;
 import org.testng.annotations.Test;
 
 public class day1 {
-
-	@Test
+	@Test(groups = {"Smoke"})
 	public void day1_1() {
 		System.out.println("day1-1");
 	}
 	@Test
 	public void day1_2() {
-		System.out.println("day1-2");
+		System.out.println("day1-2 Smoke");
 	}
 	@Test
 	public void day1_3() {
-		System.out.println("day1-3");
+		System.out.println("day1-3 ");
 	}
-	@Test
+	@Test(groups = {"Smoke"})
 	public void day1_4() {
-		System.out.println("day1-4");
+		System.out.println("day1-4 Smoke");
 	}
 
 }
--- src/test/day2.java
+++ src/test/day2.java
@@ -7,17 +7,17 @@ public class day2 {
 	public void day2_1() {
 		System.out.println("day2-1");
 	}
-	@Test
+	@Test(dependsOnMethods = {"day2_3"} )
 	public void day2_2() {
-		System.out.println("day2-2");
+		System.out.println("day2-2 ");
 	}
 	@Test
 	public void day2_3() {
-		System.out.println("day2-3");
+		System.out.println("day2-3 Depends Test");
 	}
-	@Test
+	@Test(groups = {"Smoke"})
 	public void day2_4() {
-		System.out.println("day2-4");
+		System.out.println("day2-4 Smoke");
 	}
 
 }
